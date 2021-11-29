f51d4a9b5429fe6b7b111286d2dbe22ee14d3ac1
Vu Thai Hoc <vuthaihoc.ttud@gmail.com>
2021-11-30 00:06:48 +0700

    improve download script to prevent not downloadable link

--- src/Options/DownloadUrl.php
+++ src/Options/DownloadUrl.php
@@ -23,12 +23,44 @@ module.exports = async ({ page:a, context:b }) => {
     <body><a href='#'>Link</a></body>
   </html>`);
     }
+    var requested = 0;
+    var responses = 0;
+    var have_download = false;
+    a.on('request', (request) => {
+        requested++;
+        console.log('--------', requested, responses);
+    })
+    a.on('response', (response) => {
+        responses++;
+        if(response.headers()['content-disposition'] ?? false){
+            have_download = true;
+        }
+        console.log('--------y', requested, responses, response.headers()['content-disposition'] ?? 'uuuuuu');
+    })
+    a.on('requestfailed', (request) => {
+        responses++;
+        console.log('--------n', requested, responses);
+    })
     await a.evaluate((url) => {
         var link = document.querySelector('a');
         link.href = url;
         link.click();
-        console.log('Done');
       }, b.url);
+    let pair = 0;
+    for (let i = 0; i<10; i++){
+        await a.waitForTimeout(500);
+        if(requested === responses){
+            if(requested !== pair){
+                pair = requested;                
+            }else if(pair > 0){
+                if(!have_download){
+                    return null;
+                }
+            }
+        }else{
+            pair = 0;
+        }
+    }
 };
 JS;
 
