09e89205badb904f25ee781ca4d2bf36495cc98a
vuthaihoc <vuthaihoc.ttud@gmail.com>
2024-08-09 14:37:38 +0700

    simple test for v1

--- src/Options/RequestOptions.php
+++ src/Options/RequestOptions.php
@@ -31,68 +31,94 @@ trait RequestOptions
         return $this->options[$key] ?? null;
     }
 
-    public function getOptions() : array
+    public function setOption($key, $value): self
+    {
+        $this->options[$key] = $value;
+        return $this;
+    }
+
+    public function getOptions(): array
     {
         return $this->options;
     }
 
-    public function merge(CommonOptions $option)
+    public function merge(CommonOptions $option): self
     {
         $this->options = array_merge($this->options, $option->getOptions());
         return $this;
     }
 
-    public function rejectRequestPattern() : self {return $this;}
+    public function rejectRequestPattern(): self
+    {
+        return $this;
+    }
 
     /**
      * can be 'document','stylesheet','image','media','font','script','texttrack','xhr','fetch','eventsource',
      * 'websocket','manifest','other'
      * @return $this
      */
-    public function setRejectResourceTypes(string ...$types) : self {
-        if($types[0] === null){
-            if($this->options['rejectResourceTypes'] ?? false){
+    public function setRejectResourceTypes(string ...$types): self
+    {
+        if ($types[0] === null) {
+            if ($this->options['rejectResourceTypes'] ?? false) {
                 unset($this->options['rejectResourceTypes']);
             }
-        }else{
+        } else {
             $this->options['rejectResourceTypes'] = $types;
         }
         return $this;
     }
-    public function setExtraHTTPHeaders(?array $headers) : self {
-        if($headers == null){
-            if($this->options['setExtraHTTPHeaders'] ?? false){
+
+    public function setExtraHTTPHeaders(?array $headers): self
+    {
+        if ($headers == null) {
+            if ($this->options['setExtraHTTPHeaders'] ?? false) {
                 unset($this->options['setExtraHTTPHeaders']);
             }
             return $this;
         }
 
-        if(empty($this->options['setExtraHTTPHeaders'])){
+        if (empty($this->options['setExtraHTTPHeaders'])) {
             $this->options['setExtraHTTPHeaders'] = [];
         }
 
-        foreach ($headers as $k => $header){
-            if(is_numeric($k)){
+        foreach ($headers as $k => $header) {
+            if (is_numeric($k)) {
                 list($header_name, $header_content) = explode(":", $header, 2);
                 $this->options['setExtraHTTPHeaders'][trim($header_name)] = trim($header_content);
-            }else{
+            } else {
                 $this->options['setExtraHTTPHeaders'][$k] = trim($header);
             }
         }
         return $this;
     }
-    public function setJavaScriptEnabled(?bool $enabled = null) : self {
-        if($enabled === null){
-            if($this->options['setJavaScriptEnabled'] ?? false){
+
+    public function setJavaScriptEnabled(?bool $enabled = null): self
+    {
+        if ($enabled === null) {
+            if ($this->options['setJavaScriptEnabled'] ?? false) {
                 unset($this->options['setJavaScriptEnabled']);
             }
-        }else{
+        } else {
             $this->options['setJavaScriptEnabled'] = $enabled;
         }
         return $this;
     }
-    public function userAgent() : self {return $this;}
-    public function gotoOptions() : self {return $this;}
-    public function waitFor() : self {return $this;}
+
+    public function userAgent(): self
+    {
+        return $this;
+    }
+
+    public function gotoOptions(): self
+    {
+        return $this;
+    }
+
+    public function waitFor(): self
+    {
+        return $this;
+    }
 
 }
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,5 +1,64 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+test('api/content', function () {
+    $url = "https://google.com";
+    $servers = [new \DokLibs\Browserless\Host('http://localhost:3000/')];
+    $browserless = new \DokLibs\Browserless\Client($servers);
+    $content = $browserless->content($url, new \DokLibs\Browserless\Options\DisableJavascript)->getBody()->getContents();
+    expect($content)->toContain('<body');
+});
+
+test('api/pdf', function () {
+    $url = "https://google.com";
+    $servers = [new \DokLibs\Browserless\Host('http://localhost:3000/')];
+    $browserless = new \DokLibs\Browserless\Client($servers);
+    $content = $browserless->pdf($url)->getBody()->getContents();
+    expect($content)->toStartWith('%PDF-');
+});
+
+test('api/screenshot', function () {
+    $url = "https://google.com";
+    $servers = [new \DokLibs\Browserless\Host('http://localhost:3000/')];
+    $browserless = new \DokLibs\Browserless\Client($servers);
+    $response = $browserless->screenshot($url);
+    $mime_type = $response->getHeaderLine('Content-Type');
+    expect($mime_type)->toEqual('image/jpeg');
+});
+
+test('api/download', function () {
+    $url = "https://image-us.samsung.com/SamsungUS/tv-ci-resources/2018-user-manuals/2018_UserManual_Q9FNSeries.pdf";
+    $servers = [new \DokLibs\Browserless\Host('http://localhost:3000/')];
+    $browserless = new \DokLibs\Browserless\Client($servers);
+    $response = $browserless->download($url);
+    $mime_type = $response->getHeaderLine('Content-Type');
+    expect($mime_type)->toEqual('application/pdf');
+});
+
+test('api/function', function () {
+    $url = "https://ipinfo.io/json";
+    $servers = [new \DokLibs\Browserless\Host('http://localhost:3000/')];
+    $browserless = new \DokLibs\Browserless\Client($servers);
+    $options = new \DokLibs\Browserless\Options\CommonOptions();
+    $options->setOption('code', <<<JS
+module.exports = async ({ page, context }) => {
+  const { url } = context;
+  await page.goto(url);
+  const data = await page.content();
+  return {
+    data,
+    // Make sure to match the appropriate content here
+    // You'll likely want 'application/json'
+    type: 'application/html'
+  };
+};
+JS
+    );
+    $options->setOption('context', [
+        'url' => $url,
+    ]);
+    $response = $browserless->function($url, $options);
+    $content = $response->getBody()->getContents();
+    $mime_type = $response->getHeaderLine('Content-Type');
+    expect($mime_type)->toStartWith('application/html');
+    expect($content)->toContain("ip", "city", "region", "country", "loc");
 });
